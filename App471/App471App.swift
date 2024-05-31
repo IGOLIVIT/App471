921b9c04287d2c2b567484d8faf36e2a265542df
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-05-31 18:05:09 +0300

    Initial commit

---
+++ App471/App471App.swift
@@ -0,0 +1,38 @@
+//
+//  App471App.swift
+//  App471
+//
+//  Created by IGOR on 16/04/2024.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
+@main
+struct App471App: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        
+        WindowGroup {
+            
+            NavigationView {
+                
+                ContentView()
+            }
+        }
+    }
+}
